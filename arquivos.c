1ee9a2b5c7cf13a825e70273e23ceb0ddcc28df1
magrini <magrinibruna@usp.br>
2020-05-02 14:58:08 -0300

    Inicializacao do cabecalho; Atualizacao do RRN; Atualizacao da quantidade de registros inseridos; Inicio da escrita dos registros

--- arquivos.c
+++ arquivos.c
@@ -1,20 +1,23 @@
 #include "arquivos.h"
 
+#define TAMANHO_CABECALHO_BIN 128
 #define TAMANHO_CABECALHO_CSV 89
-#define TAMANHO_REGISTRO 136 //23 (fixos) + 105 (variaveis) + 7 (virgulas) + 1 (quebra de linha)
+#define TAMANHO_REGISTRO_BIN 128 //23 bytes (fixos) + 105 (variaveis)
+#define TAMANHO_REGISTRO_CSV 136 //23 (fixos) + 105 (variaveis) + 7 (virgulas) + 1 (quebra de linha)
 #define TAMANHO_MAXIMO_REGISTRO 105 
+#define LIXO "$"
 
 typedef struct registro_cabecalho_ REGISTRO_CABECALHO;
 
 struct registro_cabecalho_ {
     char status; //ao abrir o arquivo status = 0, ao finalizar o uso status = 1
-    int RRNproxRegistro; //indica  o RRN  do  proximo  registroa  ser  inserido
+    int RRNproxRegistro; //indica  o RRN  do  proximo  registro  ser  inserido
     int numeroRegistrosInseridos;
     int numeroRegistrosRemovidos;
     int numeroRegistrosAtualizados;
 };
 
-void ler_arquivo_csv(BEBE** bebe, char registro[TAMANHO_REGISTRO]) {
+void ler_arquivo_csv(BEBE** bebe, char registro[TAMANHO_REGISTRO_CSV]) {
     int byteoffsetArquivo = 0;
     int byteoffsetRegistro = 0;
 
@@ -134,7 +137,10 @@ void ler_arquivo_csv(BEBE** bebe, char registro[TAMANHO_REGISTRO]) {
     (*bebe) = bebe_criar(idNascimento, idadeMae, dataNascimento, sexoBebe, estadoMae, estadoBebe, cidadeMae, cidadeBebe);
 }
 
-void escrevar_arquivo_bin(FILE* arquivo_gerado, BEBE* bebe) {
+void escrevar_arquivo_bin(FILE* arquivo_gerado, BEBE* bebe, int rrn_proximo_registro) {
+    fseek(arquivo_gerado, (TAMANHO_REGISTRO_BIN * rrn_proximo_registro) + TAMANHO_CABECALHO_BIN, SEEK_SET); 
+    // fwrite(bebe_get_idNascimento(bebe), sizeof(int), 1, arquivo_gerado);
+    // fwrite(bebe_get_idadeMae(bebe), sizeof(int), 1, arquivo_gerado);
     printf("idNascimeto: %d\n", bebe_get_idNascimento(bebe));
     printf("idadeMae: %d\n", bebe_get_idadeMae(bebe));
     printf("dataNascimento: %s\n", bebe_get_dataNascimento(bebe));
@@ -145,30 +151,67 @@ void escrevar_arquivo_bin(FILE* arquivo_gerado, BEBE* bebe) {
     printf("cidadeBebe: %s\n", bebe_get_cidadeBebe(bebe));
 }
 
-atualizar_status(FILE* arquivo_gerado, int status) {
+void atualizar_status(FILE* arquivo_gerado, REGISTRO_CABECALHO *registro_cabecalho, char status) {
+    (*registro_cabecalho).status = status;
     fseek(arquivo_gerado, 0, SEEK_SET);
-    frwite(&status, sizeof(int), 1, arquivo_gerado);
+    fwrite(&status, sizeof(char), 1, arquivo_gerado);
+}
+
+void atualizar_quantidade_de_registros_inseridos(FILE* arquivo_gerado, REGISTRO_CABECALHO *registro_cabecalho, int quantidade_de_registros) {
+    (*registro_cabecalho).numeroRegistrosInseridos = quantidade_de_registros; 
+    fseek(arquivo_gerado, 5, SEEK_SET);
+    fwrite(&quantidade_de_registros, sizeof(int), 1, arquivo_gerado);
+}
+
+void atualizar_rrn_proximo_registro(FILE* arquivo_gerado, REGISTRO_CABECALHO *registro_cabecalho, int rrn_proximo_registro) {
+    (*registro_cabecalho).RRNproxRegistro = rrn_proximo_registro;
+    fseek(arquivo_gerado, 1, SEEK_SET);
+    fwrite(&rrn_proximo_registro, sizeof(int), 1, arquivo_gerado);
+}
+
+void inicializar_cabecalho(FILE *arquivo_gerado, REGISTRO_CABECALHO *registro_cabecalho) {
+    int i = 0;
+    (*registro_cabecalho).status = 0;
+    (*registro_cabecalho).RRNproxRegistro = 0;
+    (*registro_cabecalho).numeroRegistrosInseridos = 0;
+    (*registro_cabecalho).numeroRegistrosRemovidos = 0;
+    (*registro_cabecalho).numeroRegistrosAtualizados = 0;
+
+    atualizar_status(arquivo_gerado, registro_cabecalho, (*registro_cabecalho).status);
+    atualizar_rrn_proximo_registro(arquivo_gerado, registro_cabecalho, (*registro_cabecalho).RRNproxRegistro);
+    atualizar_quantidade_de_registros_inseridos(arquivo_gerado, registro_cabecalho, (*registro_cabecalho).numeroRegistrosInseridos);
+    fwrite(&(*registro_cabecalho).numeroRegistrosRemovidos, sizeof(int), 1, arquivo_gerado);
+    fwrite(&(*registro_cabecalho).numeroRegistrosAtualizados, sizeof(int), 1, arquivo_gerado);
+    for(i = 0; i < 111; i++) 
+        fwrite(&LIXO, sizeof(char), 1, arquivo_gerado);   
 }
 
 void criar_arquivo(FILE* arquivo_entrada, FILE* arquivo_gerado) {
-    char cabecalho_csv[TAMANHO_CABECALHO_CSV];
-    char registro[TAMANHO_REGISTRO];
-    char *retorno;
+    REGISTRO_CABECALHO registro_cabecalho;
     BEBE* bebe;
+    char cabecalho_csv[TAMANHO_CABECALHO_CSV];
+    char registro[TAMANHO_REGISTRO_CSV];
+    char *retorno;
+    int quantidade_de_registros = 0;
 
-    atualizar_status(arquivo_gerado, 0);
-        
+    inicializar_cabecalho(arquivo_gerado, &registro_cabecalho);
+
     fgets(cabecalho_csv, TAMANHO_CABECALHO_CSV, arquivo_entrada); //Pulando linha de cabeçalho do arquivo .csv
     printf("%s", cabecalho_csv);
 
-    retorno = fgets(registro, TAMANHO_REGISTRO, arquivo_entrada);
+    retorno = fgets(registro, TAMANHO_REGISTRO_CSV, arquivo_entrada);
     while (retorno != NULL) {    
         printf("%s", registro);
         
         ler_arquivo_csv(&bebe, registro);   
-        escrevar_arquivo_bin(arquivo_gerado, bebe);
+        escrevar_arquivo_bin(arquivo_gerado, bebe, quantidade_de_registros);
+        quantidade_de_registros++;
+        atualizar_rrn_proximo_registro(arquivo_gerado, &registro_cabecalho, quantidade_de_registros);
 
         bebe_apagar(&bebe);
-        retorno = fgets(registro, TAMANHO_REGISTRO, arquivo_entrada);
+        retorno = fgets(registro, TAMANHO_REGISTRO_CSV, arquivo_entrada);
     } 
+
+    atualizar_quantidade_de_registros_inseridos(arquivo_gerado, &registro_cabecalho, quantidade_de_registros);
+    atualizar_status(arquivo_gerado, &registro_cabecalho, 1);
 }
