caa06df7a3ae30a3f7cdef1d6b18d291bb685080
magrini <magrinibruna@usp.br>
2020-05-08 21:12:07 -0300

    Erro encontrado na escrita do arquivo

--- arquivo_cabecalho.c
+++ arquivo_cabecalho.c
@@ -18,19 +18,20 @@ void inicializar_cabecalho(FILE *arquivo_gerado, REGISTRO_CABECALHO *registro_ca
 }
 
 void atualizar_status(FILE* arquivo_gerado, REGISTRO_CABECALHO *registro_cabecalho, char status) {
+    char statusInt =  '31';
     (*registro_cabecalho).status = status;
     fseek(arquivo_gerado, 0, SEEK_SET);
-    fwrite(&status, sizeof(char), 1, arquivo_gerado);
+    fwrite(&statusInt, sizeof(char), 1, arquivo_gerado);
 }
 
 void atualizar_quantidade_de_registros_inseridos(FILE* arquivo_gerado, REGISTRO_CABECALHO *registro_cabecalho, int quantidade_de_registros) {
     (*registro_cabecalho).numeroRegistrosInseridos = quantidade_de_registros; 
-    fseek(arquivo_gerado, 5, SEEK_SET);
+    fseek(arquivo_gerado, 1, SEEK_SET);
     fwrite(&quantidade_de_registros, sizeof(int), 1, arquivo_gerado);
 }
 
 void atualizar_rrn_proximo_registro(FILE* arquivo_gerado, REGISTRO_CABECALHO *registro_cabecalho, int rrn_proximo_registro) {
     (*registro_cabecalho).RRNproxRegistro = rrn_proximo_registro;
-    fseek(arquivo_gerado, 1, SEEK_SET);
+    fseek(arquivo_gerado, 4, SEEK_SET);
     fwrite(&rrn_proximo_registro, sizeof(int), 1, arquivo_gerado);
 }
