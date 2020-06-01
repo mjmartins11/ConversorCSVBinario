efc92eea204fe3a401c0e86fb36b24394700e956
magrini <magrinibruna@usp.br>
2020-05-31 21:35:19 -0300

    Finalizada operacao 6: insercao

--- main.c
+++ main.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "arquivo_cabecalho.h"
 
 int main(void) {
-    FILE* arquivo = fopen("arquivoGerado.bin", "w+b");
+    char string[100];
 
-    int zero = 0;
-    int valor = 14;
-
-    fwrite(&zero, sizeof(char), 1, arquivo);
-    fwrite(&valor, sizeof(int), 1, arquivo);
-    fwrite(&valor, sizeof(int), 1, arquivo);
-
-    fseek(arquivo, 0, SEEK_SET);
-    fread(&valor, sizeof(char), 1, arquivo);
-    printf("status: %d\n", valor);
-    fread(&valor, sizeof(int), 1, arquivo);
-    printf("rrn: %d\n", valor);
-    fread(&valor, sizeof(int), 1, arquivo);
-    printf("qtd: %d\n", valor);
+    scanf("%s", string);
+    printf("%s", string);
 
     return 0;
 }
